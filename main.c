57af6369b60efd07d44e132a0f50ab0eb1036299
Krishna Kc <kkc@c2r5p10.hive.fi>
2025-04-29 18:39:15 +0300

    update main.c entry point

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
-// main.c
 #include <stdio.h>
 
-int main() {
-    printf("Hello, Codespaces!\\n");
-    return 0;
+int main(int argc, char *argv[])
+{
+    (void)argv;
+    if (argc != 4 && argc != 5)
+    {
+        printf("Usage: ./philo <number of philo> ");
+        printf("<time to die> <time to eat>");
+        printf("<time to sleep> <optional: num of times each philo should eat>\n");
+        return (0);
+    }
+    printf("Args are valid: Check the arguments passed");
+    printf("if args are valid / Initialise struct..");
+    return (0);
 }
